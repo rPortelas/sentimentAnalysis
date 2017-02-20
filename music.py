09e6133828bc5fb62cc50488ae6cd471a02e788a
Amogh Malik <amomalik@ucdavis.edu>
2017-02-19 16:07:21 -0800

    Secure links for songs support added

--- music.py
+++ music.py
@@ -1,15 +1,20 @@
 import soundcloud
 import vlc
 
+import re
+
+
+
 # create a client object with your app credentials
 client = soundcloud.Client(client_id='WGvWDXJhUeRcmnawwdMDyTcbUmH8IHpS')
 
 
 # find all sounds of buskers licensed under 'creative commons share alike'
-tracks = client.get('/tracks', q='buskers', license='cc-by-sa')
+tracks = client.get('/tracks', q='sad instrumental', license='cc-by-sa')
 
 # fetch track to stream
-track = tracks[0]
+track = tracks[1]
+print(tracks)
 
 # get the tracks streaming URL
 stream_url = client.get(track.stream_url, allow_redirects=False)
@@ -17,5 +22,14 @@ stream_url = client.get(track.stream_url, allow_redirects=False)
 # print the tracks stream URL
 print stream_url.location
 
-p = vlc.MediaPlayer(stream_url.location)
+song = stream_url.location
+
+song = re.sub('s','',song,1)
+
+print song
+
+
+p = vlc.MediaPlayer(song)
 p.play()
+
+
