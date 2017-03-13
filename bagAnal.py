8cce3538f0409bf4e01f356998db5ce188694b11
rPortelas <remy.portelas@gmail.com>
2017-03-12 19:28:49 -0700

    emolex loaded into file

--- bagAnal.py
+++ bagAnal.py
@@ -1,20 +1,45 @@
+
+
 emotions=["anger", "anticipation", "disgust", "fear", "joy", "sadness", "trust", "surprise"]
 wordEmotions = {}
-with open("NRC-Emotion-Lexicon-Wordlevel-v0.92.txt") as f:
-    prevWord = "NULL"
-    for line in f:
-       #(key, val) = line.split()
-       #d[int(key)] = val
-       curWord = line[0]
-       emotion = line[1]
-       print(curWord)
 
-       #if current word different from the previous one,
-       #let's create a new entry in our map
-       if(curWord != prevWord):
-           wordEmotions[curWord] = [0,0,0,0,0,0,0,0]
-       print(emotion)
-       if emotion in emotions:
-           print emotions.index(emotion)
-       print line
-       prevWord = curWord
+def load_emolex(fileName):
+	#open EmoLex file and load it in wordEmotions dictionary
+	with open(fileName) as f:
+	    prevWord = "NULL"
+	    for line in f:
+	       elements = line.split()
+	       curWord = elements[0]
+	       emotion = elements[1]
+	       emotionValue = elements[2]
+	       print emotionValue
+
+	       #if current word different from the previous one,
+	       #let's create a new entry in our map
+	       if(curWord != prevWord):
+	           wordEmotions[curWord] = [0,0,0,0,0,0,0,0]
+
+	       if (emotionValue == "1") and (emotion in emotions):
+	       		#get index that we need to increment
+	       		index = emotions.index(emotion)
+
+	       		#get old array and update it
+	       		newValues = wordEmotions[curWord]
+	       		newValues[index] += 1
+	       		wordEmotions[curWord] = newValues      
+	       prevWord = curWord
+
+	#print dictionary
+	for a in wordEmotions:
+		print(a)
+		print(wordEmotions[a])
+
+
+
+#load emoLex from file
+load_emolex("NRC-Emotion-Lexicon-Wordlevel-v0.92.txt")
+
+
+
+
+
